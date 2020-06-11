0422389a4bd2c102ea91106e60fe93d2501d876f
Prabin Paudel <prabin.paudel60@gmail.com>
2020-06-11 12:49:10 +0545

    Minor Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ fetch("https://api.github.com/users/leezene")
     .then(response => response.json())
     .then(function(data){
         document.getElementById('user-avatar').src = data['avatar_url'];
+        document.getElementById('user-githublink').href = data['html_url']
         document.getElementById('user-username').textContent = `@${data['login']}`;
+       
         document.getElementById('user-fullname').textContent = data['name'];
         document.getElementById('user-followerCounts').textContent = `${data['followers']} followers`;
     });
@@ -19,13 +21,14 @@ repoRequest.onload = function () {
     if (repoRequest.status >= 200 && repoRequest.status < 400) {
         data.forEach(repo => {
             const card = document.createElement('div');
-            card.setAttribute('class', 'card width-container mx-1');
+            card.setAttribute('class', 'card card-background width-container mxy-1');
 
             const cardBody = document.createElement('div');
             cardBody.setAttribute('class', 'card-body mb-4 shadow-sm');
 
             const a = document.createElement('a');
-            Object.assign(a, {href: repo.html_url, textContent: repo.name})
+            Object.assign(a, {href: repo.html_url, textContent: repo.name});
+            a.setAttribute('class', 'repo-link');
 
             const spanDate = document.createElement('span');
             Object.assign(spanDate, {
